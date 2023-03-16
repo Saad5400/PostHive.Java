d154f701b0871ec88fc8aefb00969b25c847b14b
Saad Batwa <86385454+Saad5400@users.noreply.github.com>
2023-03-16 05:21:50 +0000

    aded user id handler class

---
+++ src/main/java/main/UserIdHandler.java
@@ -0,0 +1,31 @@
+public class UserIdHandler {
+     /**
+    * Check if a user id exists locally
+    *
+    * @return         true/false if user id exists
+    */
+    public static boolean userIdExists() {
+        // TODO: add a way to check if the user id exists
+        return false;
+    }
+
+    /**
+    * Create and store a local, unique user id
+    * 
+    * @return         the created user id
+    */
+    public static int createUserId() {
+        // TODO: write a code to generaet a unique id and save it somewhere locally
+        return 0;
+    }
+
+    /**
+    * Get the local user's id
+    *
+    * @return         the user id
+    */
+    public static int getUserId() {
+        // TODO: read and return user id that was created from createUserId()
+        return 0;
+    }
+}
